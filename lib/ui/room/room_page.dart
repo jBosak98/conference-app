642cbbcba526b7e5f98cfdf524737eaf28d69b21
jBosak <jbosak98@gmail.com>
2021-05-08 22:07:27 +0200

    long title support

--- lib/ui/room/room_page.dart
+++ lib/ui/room/room_page.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class RoomPage extends StatefulWidget{
+class RoomPage extends StatefulWidget {
   final String title;
   final String args;
-  RoomPage( {Key key, this.title, this.args}): super(key: key);
+
+  RoomPage({Key key, this.title, this.args}) : super(key: key);
 
   @override
   State<StatefulWidget> createState() => _RoomPageState();
-
 }
 
 class Event {
@@ -16,22 +16,17 @@ class Event {
   final String title;
   final String author;
   final String dateString;
-  Event(this.img, this.title, this.author,this.dateString);
+
+  Event(this.img, this.title, this.author, this.dateString);
 }
+
 class _RoomPageState extends State<RoomPage> {
   @override
   Widget build(BuildContext context) {
-
     List<Event> events = [
       Event(
         "images/me.jpg",
         "How to write Flutter app",
-         "Jakub Bosak",
-         "May 27, 6:00PM",
-      ),
-      Event(
-        "images/me.jpg",
-        "How to write Flutter app",
         "Jakub Bosak",
         "May 27, 6:00PM",
       ),
@@ -50,6 +45,12 @@ class _RoomPageState extends State<RoomPage> {
       Event(
         "images/me.jpg",
         "How to write Flutter app",
+        "Jakub Bosak",
+        "May 27, 6:00PM",
+      ),
+      Event(
+        "images/me.jpg",
+        "How to write Flutter app How to write Flutter app",
         "Jakub Bosak",
         "May 27, 6:00PM",
       ),
@@ -74,64 +75,96 @@ class _RoomPageState extends State<RoomPage> {
     ];
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xff053F5E),
-        title: Text(widget.title),
+        appBar: AppBar(
+          backgroundColor: Color(0xff053F5E),
+          title: Text(widget.title),
           actions: <Widget>[
             IconButton(
                 icon: Icon(Icons.message),
-                onPressed: () => Navigator.pushNamed(context, '/chatLobby')
-            ),
+                onPressed: () => Navigator.pushNamed(context, '/chatLobby')),
             IconButton(
-              icon: Icon(Icons.account_circle),
-              onPressed: () => Navigator.pushNamed(context, '/login')
-            ),
+                icon: Icon(Icons.account_circle),
+                onPressed: () => Navigator.pushNamed(context, '/login')),
           ],
-      ),
-      body:
-          Container(
+        ),
+        body: Container(
             decoration: BoxDecoration(
-              color: Colors.grey[100],
-              borderRadius: BorderRadius.only(
-                topRight: Radius.circular(30), topLeft: Radius.circular(30)
-              )
-            ),
-              child:Column(
-                children: [
-                  Expanded(
-                      child: Container(
-                          margin: EdgeInsets.only(left: 20, right: 20),
-                          child:
-                          ListView(
-                              children: [
-                                ...events.map<Widget>((event) =>
-                                    eventComponent(
-                                        event.img,
-                                        event.title,
-                                        event.author,
-                                        event.dateString
-                                    )
-                                ).toList()
-                              ]
-                          )
-                      )
-                  )
-                ],
-              )
-          )
-    );
+                color: Colors.grey[100],
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30),
+                    topLeft: Radius.circular(30),
+                )),
+            child: Column(
+
+              children: [
+                Expanded(
+
+                    child: Container(
+                        margin: EdgeInsets.only(left: 20, right: 20),
+                        child: ListView(children: [
+                          ...events
+                              .map<Widget>((event) =>
+                              eventComponent(event.img,
+                                  event.title, event.author, event.dateString))
+                              .toList()
+                        ])))
+              ],
+            )));
   }
 
-  Widget eventComponent(
-      String img,
-      String name,
-      String author,
-      String dateString
-      ) {
-    final size = MediaQuery.of(context).size;
+  Widget eventComponentRightSite(String name, String author,
+      String dateString) {
+    final size = MediaQuery
+        .of(context)
+        .size;
+    return Flexible(
+        child: Container(
+            margin: EdgeInsets.only(left: 20, top: 10, bottom: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: Text(name,
+                        style: TextStyle(
+                          color: Color(0xff363636),
+                          fontSize: 17,
+                          fontFamily: 'Roboto',
+                          fontWeight: FontWeight.w700,
+                        ))),
+                Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: Row(children: [
+                      Text(author,
+                          style: TextStyle(
+                            color: Color(0xffababab),
+                            fontFamily: 'Roboto',
+                            fontWeight: FontWeight.w300,
+                          )),
+                      Container(
+                          margin: EdgeInsets.only(
+                              top: 3, left: size.width * 0.15),
+                          child: Row(children: [
+                            Container(
+                                child: Text(dateString,
+                                    style: TextStyle(
+                                        color: Colors.black,
+                                        fontSize: 12,
+                                        fontFamily: 'Roboto')))
+                          ]))
+                    ]))
+              ],
+            )));
+  }
 
-    return Container(
-        height: 90,
+  Widget eventComponent(String img, String name, String author,
+      String dateString) {
+    return Row(
+
+        children:[Flexible(
+        child:Container(
+//        height: 90,
         margin: EdgeInsets.only(top: 10),
         decoration: BoxDecoration(
           color: Colors.white,
@@ -145,63 +178,10 @@ class _RoomPageState extends State<RoomPage> {
                 margin: EdgeInsets.only(left: 20),
                 height: 90,
                 width: 50,
-                child: Image.asset(img)
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 20, top: 10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                    margin:EdgeInsets.only(top: 10),
-                    child: Text(
-                      name,
-                      style: TextStyle(
-                        color: Color(0xff363636),
-                        fontSize: 17,
-                        fontFamily: 'Roboto',
-                        fontWeight: FontWeight.w700
-                      )
-                    )
-                  ),
-                  Container(
-                    margin: EdgeInsets.only(top: 10),
-                    child: Row(
-                      children: [
-                        Text(
-                            author,
-                            style: TextStyle(
-                              color: Color(0xffababab),
-                              fontFamily: 'Roboto',
-                              fontWeight: FontWeight.w300,
-                            )
-                        ),
-                        Container(
-                          margin: EdgeInsets.only(top: 3, left: size.width * 0.15),
-                          child: Row(
-                            children:[
-                              Container(
-                                child: Text(
-                                    dateString,
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 12,
-                                        fontFamily: 'Roboto'
-                                    )
-                                )
-                              )
-                            ]
-                          )
-                        )
-                      ]
-                    )
-                  )
-                ],
-              )
-            )
+                child: Image.asset(img)),
+            eventComponentRightSite(name, author, dateString)
+
           ],
-        )
-    );
+        )))]);
   }
 }
